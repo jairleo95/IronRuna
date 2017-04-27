f5c5ccaa03ae67d0f2448605876e017aa3e0757f
Jair Santos <Jair Santos>
2017-04-27 10:15:34 -0500

    hello git

--- resources/views/webPage/checkout.blade.php
+++ resources/views/webPage/checkout.blade.php
@@ -24,59 +24,18 @@
 									<th>Total</th>
 								</tr>
 							</thead>
-							<tbody>
-								<tr>
-									<td>
-										<div class="form-check">
-											<label class="form-check-label">
-												<input type="radio" class="form-check-input" name="optionsRadios" id="optionsRadios2" value="option2">
-											</label>
-										</div>
-									</td>
-									<td>
-										<i class="fa fa-user fa-2x"></i>
-										<span>Venta Individuales</span>
-									</td>
-									</td>
-									<td>S/.124.99</td>
-									<td><input type="text" class="form-control spinner" name="quantity" value="1"/></td>
-									<td data-title="Total">S/.124.99</td>
-								</tr>
-								<tr>
-									<td>
-										<div class="form-check">
-											<label class="form-check-label">
-												<input type="radio" class="form-check-input" name="optionsRadios" id="optionsRadios2" value="option2">
-											</label>
-										</div>
-									</td>
-									<td>
-										<i class="fa fa-users fa-2x"></i>
-										<span>Venta Equipos (Postas)</span>
-									</td>
-									<td data-title="Unit Price">S/.124.99</td>
-									<td>
-										<input type="text" name="quantity"   value="1" class="form-control spinner"/>
-									</td>
-									<td data-title="Total">S/.124.99</td>
-								</tr>
-							</tbody>
+							<tbody class="tableTicketDetails"></tbody>
 						</table>
 						<div class="cart-foot row">
 								<div class="col-sm-6">
  						<p>Make your payment directly into our bank account. Please use your Order ID as the payment reference. Your order won’t be shipped until the funds have cleared in our account.</p>
-
 								</div>
 								<div class="col-sm-6 cart-total">
-									<div class="cart-subtotal">
-										<span>Subtotal</span>
-										<span>S/.369.97</span>
-									</div>
 									<div class="order-total">
 										<strong>Total del pedido</strong>
-										<strong>S/.369.97</strong>
+										<strong>S/.<span class="totalCost">0.0</span></strong>
 									</div>
-									<button class="btn btn-success pull-right" type="submit">
+									<button class="btn btn-success pull-right payButton" type="submit">
 										<i class="fa fa-send"></i>
 										<span>Pagar Ahora</span>
 									</button>
@@ -89,14 +48,29 @@
 		</div>
 			<input type="text" class="eventSelected" value="{{ session('eventSelected')}}" >
 		@endsection
-@section('scripts')
-    <script>
-        $( ".spinner" ).spinner({
-			min:0,
-			max:3,
-			down: "ui-icon-triangle-1-s",
-			up: "ui-icon-triangle-1-n"
-        });
+		@section('scripts')
+    		<script>
+                var totalCost=0.0;
+                var objTotalCost=$('.totalCost');
+				function calcTotalCostByEvent(itemQuantity,itemPrice){
+					return itemQuantity*itemPrice;
+				}
+		function initTickesEventElements(){
+            $( ".itemQuantity" ).spinner({
+                min:0,
+                max:3,
+                down: "ui-icon-triangle-1-s",
+                up: "ui-icon-triangle-1-n",
+                change: function( event, ui ) {
+                    var itemQuantity=$(this).spinner( "value" );
+                  //  $(this).parents('tr').find($('.itemPrice')).css('background','red');
+                    var itemPrice=$(this).parents('tr').find($('.itemPrice')).val();
+                    console.log('itemPrice:'+itemPrice)
+                    totalCost =calcTotalCostByEvent(itemQuantity,itemPrice);
+                    objTotalCost.text(totalCost);
+				}
+            });
+		}
 
         function getCostByIdData(event,callback){
 			$.ajax({
@@ -113,19 +87,41 @@
 		function buildTicket(tableTicketDetails,eventSelected){
             tableTicketDetails.empty();
             getCostByIdData(eventSelected,function(data){
-                var htmlString = '';
+                var hs = '';
                 if (data.status){
                     var list = data.list;
-                    for (  var i =0; i<list.lenght;i++){
-					console(list[i].value);
+                    for (  var i =0; i<list.length;i++){
+					console.log((list[i].value));
+                        hs += '<tr>';
+                        hs += '  <td>';
+                        hs += '<div class="form-check">';
+                        hs += '  <label class="form-check-label">';
+                        hs += ' <input type="radio" class="form-check-input" name="optionsRadios" id="optionsRadios2" value="option2">';
+                        hs += '  </label>';
+                        hs += '  </div>';
+                        hs += ' </td>';
+                        hs += '  <td>';
+                        hs += ' <i class="fa fa-user fa-2x"></i>';
+                        hs += '  <span>'+(list[i].name)+'</span>';
+                        hs += '  </td>';
+                        hs += '  </td>';
+                        hs += ' <td>'+(list[i].value)+'</td>';
+                        hs += ' <td><input type="text" class="form-control itemQuantity" name="quantity" value="0"/></td>';
+                        hs += '   <td data-title="Total">S/. <span class="totalITem">0.0</span><input class="itemPrice" value="'+(list[i].value)+'" type="text" /></td>';
+                        hs += '  </tr>';
+
                     }
+                    tableTicketDetails.append(hs);
+					initTickesEventElements();
 				} else{
                     console('something was wrong');
 				}
 			});
 		}
-        var tableTicketDetails=$('tableTicketDetails');
+        var tableTicketDetails=$('.tableTicketDetails');
         var event =$('.eventSelected').val();
+
+        var btnPay=$('.payButton');
         buildTicket(tableTicketDetails,event);
     </script>
     @endsection
