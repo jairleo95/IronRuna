92fab5e068c6abf645f671fdefc5be3e418da068
Jair Santos <Jair Santos>
2017-04-28 16:54:46 -0500

    asda

--- database/migrations/2017_03_30_012819_create_cost_table.php
+++ database/migrations/2017_03_30_012819_create_cost_table.php
@@ -19,6 +19,7 @@ class CreateCostTable extends Migration
             $table->string('name',100);
             $table->double('value',10,2);
             $table->boolean('recordStatus');
+            $TABLE->boolean('costType');
             $table->foreign('idEvent')->references('idEvent')->on('event');
             $table->timestamps();
         });
